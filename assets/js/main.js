d3369d16f126aa8318b4797b2a79f231e0e3fe57
Claud Watari <claudwatari95@gmail.com>
2023-09-10 13:54:55 +0300

    fix: smart text generator - portfolio item - target

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,7 +145,7 @@
             "title": "Smart Text Generator",
             "description": "A chatbot using GPT Turbo 3.5 (NodeJs, Handlebars) built as part of internal coding challenges for workplace teams.",
             "image": "blank",
-            "link": "https://github.com/claudwatari95/"
+            "link": "https://github.com/ClaudWatari95/smart-text-generator"
         },
         {
             "title": "Car Booking API",
