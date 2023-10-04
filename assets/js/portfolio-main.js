21493fafbc2d1f9fd966a08c76b6e10ac9b59f88
ClaudWatari <claudwatari95@gmail.com>
2023-10-04 16:22:26 +0300

    fix: images target

--- assets/js/portfolio-main.js
+++ assets/js/portfolio-main.js
@@ -3,19 +3,19 @@ const portfolio_list = [
   {
     "title": "ProLearnerHub",
     "description": "An eLearning Platform where anyone can learn the fundamentals of Computer Science",
-    "image": "../assets/images/code.png",
+    "image": "https://img.freepik.com/free-vector/digital-global-connection-network-technology-background_1017-23324.jpg",
     "link": "https://prolearnerhub.tech",
   },
   {
     "title": "Smart Text Generator",
     "description": "A chatbot built on GPT4 OpenAI model as part of team coding challenges",
-    "image": "../assets/images/chatbot.png",
+    "image": "https://img.freepik.com/premium-photo/system-artificial-intelligence-chatgpt-ai-technology-smart-robot-ai-chat-application-software-robot-application-chat-gpt-generative-ai_39665-590.jpg",
     "link": "https://smart-text-generator-production.up.railway.app/chat",
   },
   {
     "title": "Notes App",
     "description": "A notes tracking native app as part of team coding challenges",
-    "image": "../assets/images/notes.png",
+    "image": "https://img.freepik.com/free-vector/business-infographic-with-pinned-notes_23-2147505469.jpg",
     "link": "https://github.com/ClaudWatari95/notes_app",
   },
 ];
